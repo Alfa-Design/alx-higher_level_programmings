e34131f7cdbeb58510f218d9f8c216b378355350
John Ouma <john7ouma@Gmail.com>
2023-08-10 22:56:44 -0700

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+#Subtraction
+def sub(a, b):
+    return (a - b)
+
+#Addition
+def add(a, b):
+    return (a + b)
+
+#multiplication
+def mul(a, b):
+    return (a * b)
+
+#Divition
+def div(a, b):
+    return (a/b)
---
+++ 0x02-python-import_modules/variable_load_5.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+#simple variable
+a = 98
